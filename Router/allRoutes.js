3ba70ce3e091b73735cda92b8704057d1b54adb6
Narcisse <nkokoua0@gmail.com>
2024-06-10 22:42:39 +0200

    Bug_ok

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,9 +1,12 @@
+// Import the Route class from Route.js
 import Route from "./Route.js";
 
-//Définir ici vos routes
+// Define your routes here
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", "index.html"),
-    new Route("/", "habitat", "/pages/habitat.html"),];
+    new Route("/habitat", "Habitat", "/pages/habitat.html"),
+    new Route("/signin", "Connexion/Deconnexion", "/pages/signin.html")
+  ];
 
-//Le titre s'affiche comme ceci : Route.titre - websitename
+// The title is displayed as follows: Route.titre - websitename
 export const websiteName = "Arcadia";
