5d744e9bb705a9abdfefe56073bc2cc5165c3ae1
Narcisse <nkokoua0@gmail.com>
2024-06-22 09:58:58 +0000

    Création pages habitas

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", "index.html"),
     new Route("/habitat", "Habitat", "/pages/habitat.html"),
-    new Route("/signin", "Connexion/Deconnexion", "/pages/signin.html")
+    new Route("/signin", "Connexion/Deconnexion", "/pages/signin.html"),
+    new Route("/Forêt", "forêt", "/pages/Forêt.html"),
+    new Route("/Air", "air", "/pages/Air.html"),
+    new Route("/Rivière", "rivière", "/pages/Rivière.html"),
   ];
 
 // The title is displayed as follows: Route.titre - websitename
